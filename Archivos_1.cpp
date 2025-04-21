35a3fdb276f553bf985199531ba30f9871c62dd8
laje-E <136092273+laje-E@users.noreply.github.com>
2025-04-21 13:52:37 -0300

    Add files via upload

---
+++ Archivos_1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main () {
+	char apellido [20];
+	printf ("ingrese su apellido ");
+	scanf ("%s", apellido);
+	
+	char nombre [20];
+	printf ("ingrese su nombre ");
+	scanf ("%s", nombre);
+	
+	int DNI;
+	printf ("ingrese el numero de su DNI ");
+	scanf ("%d", DNI);
+	
+	
+}

45f8f1ba2804eb1c9ac71efefb9dedf84c494c4a
laje-E <136092273+laje-E@users.noreply.github.com>
2025-04-23 09:04:48 -0300

    Add files via upload

--- Archivos_1.cpp
+++ Archivos_1.cpp
@@ -8,7 +8,16 @@ int main (int argc, char **argv) {
 	char nombre [20];
 	
 	char DNI [8];
-	
+    
+    int opcion;
+    
+    printf ("ingrese que opcion quiere efectuar: 0= salir | 1= ingresar datos | 2= buscar DNI ");
+    scanf ("%d", &opcion);
+    
+    while (opcion != 0){
+    
+        if (opcion == 1){
+    
 	FILE *ingresar_datos;
 	ingresar_datos = fopen("archivo.txt", "a+");
 	if (ingresar_datos == NULL){
@@ -29,6 +38,7 @@ int main (int argc, char **argv) {
 	    scanf ("%s", DNI);
 	    
 	    
+	    
 	    /* pero si queremos utilizar el DNI como int hay que hacer esto
 	    fscanf (ingresar_datos, "%d", &DNI);
 	    printf ("ingrese el numero de su DNI ");
@@ -39,9 +49,9 @@ int main (int argc, char **argv) {
 	    /*convertir el DNI en string para que asi se pueda leer en el fputs*/
 	    
 	    fputs(apellido,ingresar_datos);
-	    fputs ("\n", ingresar_datos);
+	    fputs (" ", ingresar_datos);
 	    fputs(nombre,ingresar_datos);
-	    fputs ("\n", ingresar_datos);
+	    fputs (" ", ingresar_datos);
 	    fputs(DNI,ingresar_datos);
 	    fputs ("\n", ingresar_datos);
 	    fputs ("\n", ingresar_datos);
@@ -49,8 +59,33 @@ int main (int argc, char **argv) {
 	}
 	
 	fclose(ingresar_datos);
+	
+    }
+	
+	if (opcion == 2){
+	char busqueda [9];
+	printf ("ingrese un DNI que quiera buscar");
+	scanf ("%s", busqueda);
+	
+	FILE *buscar_datos;
+	buscar_datos = fopen ("archivo.txt", "r");
+	
+	if (buscar_datos == NULL){
+		printf ("error al abrir el archivo ");
+	}
+	
+	else {
+	    if (strcmp (DNI, busqueda) == 0){
+	        printf ("el nombre del dni buscado es: %s ", nombre);
+	        printf (" y el apellido es: %s ", apellido);
+	    }
+	    else if (strcmp (DNI, busqueda) != 0){
+	        printf ("el numero de DNI buscado no esta almacenado en este archivo ");
+	    }
+	}
+	}
+	printf ("ingrese que opcion quiere efectuar: 0= salir | 1= ingresar datos | 2= buscar DNI ");
+	scanf ("%d", &opcion);
+    }
 	return 0;
-	
-	
-	
 }
